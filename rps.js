758ff1712a4894b5f8ea4de8de4489754a433e06
Hathai123 <126381627+Hathai123@users.noreply.github.com>
2026-06-14 17:03:08 +0700

    add getHumanChoice() in rps.js

--- rps.js
+++ rps.js
@@ -1,6 +1,24 @@
 //pseudocode Rock Paper Scissors Game
 //get player's answer between Rock Paper Scissors
+console.log(getHumanChoice());
+function getHumanChoice(){
+    //get player's choice by prompt
+    let answer = parseInt(prompt(`Please select your choice
+        enter 1 if you want Rock
+        enter 2 if you want Paper
+        enter 3 if you want Scissors
+        Ans: `));
+    switch (answer) {
+        case 1:
+            return "Rock";
+        case 2:
+            return "Paper";
+        case 3:
+            return "Scissors";
+    }
 
+
+}
 //get computer's answer by random between Rock Paper Scissors
 console.log(getComputerChoice());
 function getComputerChoice() {
