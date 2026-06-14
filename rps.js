bafa2550beb8149625f6ea57d338e298922e90c2
Hathai123 <126381627+Hathai123@users.noreply.github.com>
2026-06-14 16:29:41 +0700

    Add pseudocode in rps.js

---
+++ rps.js
@@ -0,0 +1,5 @@
+//pseudocode Rock Paper Scissors Game
+//get player's answer between Rock Paper Scissors
+//get computer's answer by random between Rock Paper Scissors
+//compare player's answer and computer's answer who will win
+//declare result
